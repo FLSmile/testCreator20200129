b04a86c4f3c827a013db508647e4ad6538b27b27
312939427@qq.com <Ll2235426>
2020-01-29 21:58:33 +0800

    first version

---
+++ web-mobile/src/settings.7d8da.js
@@ -0,0 +1,141 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {},
+        internal: {
+            "14TDKXr2NJ6LjvHPops74o": [
+                "effects/builtin-2d-gray-sprite.effect",
+                "cc.EffectAsset"
+            ],
+            "0ek66qC1NOQLjgYmi04HvX": [
+                "effects/builtin-2d-spine.effect",
+                "cc.EffectAsset"
+            ],
+            "28dPjdQWxEQIG3VVl1Qm6T": [
+                "effects/builtin-2d-sprite.effect",
+                "cc.EffectAsset"
+            ],
+            c0BAyVxX9JzZy8EjFrc9DU: [
+                "effects/builtin-clear-stencil.effect",
+                "cc.EffectAsset"
+            ],
+            "796vrvt+9F2Zw/WR3INvx6": [
+                "effects/builtin-unlit-transparent.effect",
+                "cc.EffectAsset"
+            ],
+            "6dkeWRTOBGXICfYQ7JUBnG": [
+                "effects/builtin-unlit.effect",
+                "cc.EffectAsset"
+            ],
+            "6fgBCSDDdPMInvyNlggls2": [
+                "materials/builtin-2d-base.mtl",
+                "cc.Material"
+            ],
+            "3ae7efMv1CLq2ilvUY/tQi": [
+                "materials/builtin-2d-gray-sprite.mtl",
+                "cc.Material"
+            ],
+            "7a/QZLET9IDreTiBfRn2PD": [
+                "materials/builtin-2d-spine.mtl",
+                "cc.Material"
+            ],
+            "ecpdLyjvZBwrvm+cedCcQy": [
+                "materials/builtin-2d-sprite.mtl",
+                "cc.Material"
+            ],
+            cffgu4qBxEqa150o1DmRAy: [
+                "materials/builtin-clear-stencil.mtl",
+                "cc.Material"
+            ],
+            "2aKWBXJHxKHLvrBUi2yYZQ": [
+                "materials/builtin-unlit.mtl",
+                "cc.Material"
+            ]
+        }
+    },
+    launchScene: "db://assets/Scene/helloworld.fire",
+    scenes: [
+        {
+            url: "db://assets/Scene/helloworld.fire",
+            uuid: "2dL3kvpAxJu6GJ7RdqJG5J"
+        }
+    ],
+    packedAssets: {
+        "0628b28af": [
+            "02delMVqdBD70a/HSD99FK",
+            "28dPjdQWxEQIG3VVl1Qm6T",
+            "29FYIk+N1GYaeWH/q1NxQO",
+            "2dL3kvpAxJu6GJ7RdqJG5J",
+            "31vIlawANFZqnzLlSuHBfc",
+            "41D7kWhyFGY7q4NDlzkazn",
+            "6aoKpq6+5BVaCIpoemqt7E",
+            "71VhFCTINJM6/Ky3oX9nBT",
+            "a2MjXRFdtLlYQ5ouAFv/+R",
+            "a8Anh32NZGRZegUtSgEj26",
+            "b4P/PCArtIdIH38t6mlw8Y",
+            "e8Ueib+qJEhL6mXAHdnwbi",
+            "e97GVMl6JHh5Ml5qEDdSGa",
+            "ecpdLyjvZBwrvm+cedCcQy",
+            "f0BIwQ8D5Ml7nTNQbh1YlS"
+        ],
+        "079499991": [
+            "2aKWBXJHxKHLvrBUi2yYZQ",
+            "6dkeWRTOBGXICfYQ7JUBnG"
+        ],
+        "07ce7530a": [
+            "14TDKXr2NJ6LjvHPops74o",
+            "3ae7efMv1CLq2ilvUY/tQi"
+        ],
+        "0d669730c": [
+            "c0BAyVxX9JzZy8EjFrc9DU",
+            "cffgu4qBxEqa150o1DmRAy"
+        ],
+        "0e4bc3b03": [
+            "0ek66qC1NOQLjgYmi04HvX",
+            "7a/QZLET9IDreTiBfRn2PD"
+        ]
+    },
+    md5AssetsMap: {
+        import: [
+            "0628b28af",
+            "c1175",
+            "079499991",
+            "76473",
+            "07ce7530a",
+            "f5ba6",
+            "0d669730c",
+            "1db61",
+            "0e4bc3b03",
+            "29db7",
+            "6fgBCSDDdPMInvyNlggls2",
+            "58d0e",
+            "796vrvt+9F2Zw/WR3INvx6",
+            "1cc93"
+        ],
+        "raw-assets": [
+            "02delMVqdBD70a/HSD99FK",
+            "cea68",
+            "6aoKpq6+5BVaCIpoemqt7E",
+            "55ea4",
+            "71VhFCTINJM6/Ky3oX9nBT",
+            "c06a9",
+            "a8Anh32NZGRZegUtSgEj26",
+            "cea68",
+            "b4P/PCArtIdIH38t6mlw8Y",
+            "83fcc",
+            "e8Ueib+qJEhL6mXAHdnwbi",
+            "90cf4"
+        ]
+    },
+    orientation: "",
+    debug: true,
+    subpackages: {}
+};
